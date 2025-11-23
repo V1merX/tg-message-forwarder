58d5d5a43ec62df0b8513edd67c697d2c946f3d0
V1merX <vimertop73@gmail.com>
2025-11-23 15:06:04 +0300

    add main.go file

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,3 +1,18 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+
+	"github.com/V1merX/tg-message-forwarder/internal/app"
+)
+
+func main() {
+	ctx := context.Background()
+
+	a := app.New()
+
+	if err := a.Run(ctx); err != nil {
+		log.Fatal("Failed to start app")
+	}
+}
