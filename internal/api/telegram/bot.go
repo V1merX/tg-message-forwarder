ed067a9d9773cbdd20228544272ef2ceaabf41d2
V1merX <vimertop73@gmail.com>
2025-11-23 17:04:08 +0300

    add telegram package

--- internal/api/telegram/bot.go
+++ internal/api/telegram/bot.go
@@ -1,1 +1,66 @@
 package telegram
+
+import (
+	"context"
+	"log/slog"
+
+	"github.com/mymmrac/telego"
+	th "github.com/mymmrac/telego/telegohandler"
+	tu "github.com/mymmrac/telego/telegoutil"
+)
+
+type Bot struct {
+	token string
+	log   *slog.Logger
+}
+
+func NewBot(log *slog.Logger, token string) *Bot {
+	return &Bot{
+		log:   log,
+		token: token,
+	}
+}
+
+func (b *Bot) Start(ctx context.Context) error {
+	bot, err := telego.NewBot(b.token, telego.WithDefaultDebugLogger())
+	if err != nil {
+		b.log.Error("Failed to start telegram bot", slog.Any("err", err))
+		return err
+	}
+
+	updates, err := bot.UpdatesViaLongPolling(ctx, nil)
+	if err != nil {
+		b.log.Error("Failed to get updates via long polling", slog.Any("err", err))
+		return err
+	}
+
+	bh, err := th.NewBotHandler(bot, updates)
+	if err != nil {
+		b.log.Error("Failed to create bot handler", slog.Any("err", err))
+		return err
+	}
+
+	defer func() {
+		if err := bh.Stop(); err != nil {
+			b.log.Error("Failed to stop bot handler", slog.Any("err", err))
+			return
+		}
+	}()
+
+	bh.Handle(func(ctx *th.Context, update telego.Update) error {
+		stdCtx := ctx.Context()
+
+		_, err := ctx.Bot().SendMessage(stdCtx, tu.Message(
+			tu.ID(update.Message.Chat.ID),
+			"Unknown command, use /start",
+		))
+		return err
+	}, th.AnyCommand())
+
+	b.log.Debug("Start handling updates")
+	if err := bh.Start(); err != nil {
+		return err
+	}
+
+	return nil
+}
--- internal/app/di.go
+++ internal/app/di.go
@@ -1,7 +1,73 @@
 package app
 
-type diContainer struct{}
+import (
+	"context"
+	"log/slog"
+	"os"
+
+	"github.com/V1merX/tg-message-forwarder/config"
+	"github.com/V1merX/tg-message-forwarder/internal/api/telegram"
+	kfk "github.com/V1merX/tg-message-forwarder/internal/repository/kafka"
+	"github.com/segmentio/kafka-go"
+)
+
+type diContainer struct {
+	kafkaConn *kafka.Conn
+	bot       *telegram.Bot
+	log       *slog.Logger
+	config    *config.Config
+}
 
 func NewDIContainer() *diContainer {
 	return &diContainer{}
+}
+
+func (d *diContainer) InitKafka(ctx context.Context) (*kafka.Conn, error) {
+	var err error
+	if d.kafkaConn == nil {
+		d.kafkaConn, err = kfk.Open(ctx)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return d.kafkaConn, nil
+}
+
+func (d *diContainer) InitConfig() (*config.Config, error) {
+	var err error
+	if d.config == nil {
+		d.config, err = config.MustLoad()
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return d.config, nil
+}
+
+func (d *diContainer) InitLogger() (*slog.Logger, error) {
+	if d.log == nil {
+		d.log = slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{
+			Level: slog.LevelDebug,
+		}))
+	}
+
+	return d.log, nil
+}
+
+func (d *diContainer) InitBot() (*telegram.Bot, error) {
+	if d.bot == nil {
+		logger, err := d.InitLogger()
+		if err != nil {
+			return nil, err
+		}
+		config, err := d.InitConfig()
+		if err != nil {
+			return nil, err
+		}
+		d.bot = telegram.NewBot(logger, config.Bot.Token)
+	}
+
+	return d.bot, nil
 }
